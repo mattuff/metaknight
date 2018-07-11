11eadd09f41345b769c10ebe4a3dae8a1c5ae487
Matt U <matt.uff@gmail.com>
2018-07-11 15:29:14 -0700

    updating strand

--- strandClass.py
+++ strandClass.py
@@ -1,13 +1,26 @@
-class arc:
-    def __init__(self,name,knot,succ):
+class strand:
+    
+    def __init__(self,name,component,succ,pred):
         self.name=name
         self.knot=knot
         self.succ=succ
+        
     def set_succ(self,succ):
         self.succ=succ
+        
     def get_succ(self):
         return(self.succ)
-    def get_knot(self):
-        return(self.knot)
+        
+    def set_pred(self,pred):
+        self.pred=pred
+        
+    def get_pred(self):
+        return(self.pred)
+    
+    def get_component(self):
+        return(self.component)
+    
+    def set_component(self):
+        return(self.component)
 
 arc1 = arc("Matt","Trefoil","Succ")
