6656f2407e5319be4055813454ad1c79986018d9
Matt U <matt.uff@gmail.com>
2018-07-11 15:25:03 -0700

    Added strand class

---
+++ strandClass.py
@@ -0,0 +1,13 @@
+class arc:
+    def __init__(self,name,knot,succ):
+        self.name=name
+        self.knot=knot
+        self.succ=succ
+    def set_succ(self,succ):
+        self.succ=succ
+    def get_succ(self):
+        return(self.succ)
+    def get_knot(self):
+        return(self.knot)
+
+arc1 = arc("Matt","Trefoil","Succ")
