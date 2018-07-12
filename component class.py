a50e4ce071e94cbdf6be68e9a43d30f7602a62c9
lauraelst <41130229+lauraelst@users.noreply.github.com>
2018-07-12 16:16:10 -0700

    Update component class.py

--- component class.py
+++ component class.py
@@ -1,14 +1,14 @@
 class component:
 
-    def __init__(self, handle, framing):
+    def __init__(self, handle, framing): #for 1-handles, framing=infinity
         self.handle = handle
         self.framing = framing
 
-    def getHandle(self):
+    def getHandle(self): #returns type of handle: one or two handle
         return self.handle
 
-    def getFraming(self):
+    def getFraming(self): #returns framing of 2-handles
         return self.framing
 
-    def changeFraming(self, f):
+    def changeFraming(self, f): #changes framing of 2-handles
         framing=f
