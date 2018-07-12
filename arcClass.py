620271282a036cafb1d1ed2acef9daa1925aab6f
lauraelst <41130229+lauraelst@users.noreply.github.com>
2018-07-12 09:29:32 -0700

    Update arcClass.py

--- arcClass.py
+++ arcClass.py
@@ -1,8 +1,8 @@
 class arc:
     def __init__(self,name,knot,succ):
-        self.name=name
+        self.name=name #is this necessary?
         self.knot=knot
-        self.succ=succ
+        self.succ=succ #succ needs to be an arc already; doesn't work: one of them has to be the first to be defined.
     def set_succ(self,succ):
         self.succ=succ
     def get_succ(self):
