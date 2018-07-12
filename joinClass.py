6fdebb506a463f19dcf0ec86fd6fad699b95a425
lauraelst <41130229+lauraelst@users.noreply.github.com>
2018-07-12 16:17:49 -0700

    Update joinClass.py

--- joinClass.py
+++ joinClass.py
@@ -1,16 +1,16 @@
 class join:
     
-    def __init__(self,strand0,strand1):
+    def __init__(self,strand0,strand1): #sets up join from 2 strands
         self.strands=[strand0,strand1]
 
-    def __getitem__(self,key):
+    def __getitem__(self,key): #returns either 0th strand or 1st strand
         return(self.strands[key])
 
-    def __contains__(self,item):
+    def __contains__(self,item): #returns true/false if strand is in join
         return(item in self.strands)
 
-    def setStrands(self,strand0,strand1):
+    def setStrands(self,strand0,strand1): #sets strands for join
         self.strands=[strand0,strand1]
 
-    def getStrands(self):
+    def getStrands(self): #returns strands for join as a list
         return(self.strands)
