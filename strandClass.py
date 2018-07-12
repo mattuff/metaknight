3c282ecbfcdd8ef666b8752afd0342fc95a8864d
Matt U <matt.uff@gmail.com>
2018-07-12 16:20:31 -0700

    commented strand class

--- strandClass.py
+++ strandClass.py
@@ -1,24 +1,24 @@
 class strand:
     
-    def __init__(self,component,succ=None,pred=None):
-        self.component=component
+    def __init__(self,comp,succ=None,pred=None):
+        self.comp=comp
         self.succ=succ
         self.pred=pred
         
-    def set_succ(self,succ):
+    def set_succ(self,succ): #set the successor of this strand to be the strand succ
         self.succ=succ
         
-    def get_succ(self):
+    def get_succ(self): #return the successor of this strand
         return(self.succ)
         
-    def set_pred(self,pred):
+    def set_pred(self,pred): #set the predecessor of this strand to be the strand succ
         self.pred=pred
         
-    def get_pred(self):
+    def get_pred(self): #return the successor of this strand
         return(self.pred)
     
-    def get_component(self):
-        return(self.component)
+    def set_component(self,comp): #set the component to which this strand belongs to be the component comp
+        self.comp=comp
     
-    def set_component(self):
-        return(self.component)
+    def get_component(self): #return the component to which this strand belongs
+        return(self.comp)
