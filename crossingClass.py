622f6cb0fb72223bb11480e1bdfe1fcf44907112
Laura Stordy <laurastordy@Lauras-MacBook-Pro.local>
2018-07-13 16:11:36 -0700

    changed method names to be more consistant

--- crossingClass.py
+++ crossingClass.py
@@ -9,8 +9,8 @@ class crossing:
     def __contains__(self,item): #returns boolean- True if 'item' is in the crossing
         return(item in self.strands)
 
-    def setStrands(self,strand0,strand1,strand2,strand3): #define new strands that come out of crossing
+    def set_strands(self,strand0,strand1,strand2,strand3): #define new strands that come out of crossing
         self.strands=[strand0,strand1,strand2,strand3] 
 
-    def getStrands(self): #returns the strands
+    def get_strands(self): #returns the strands
         return(self.strands)
