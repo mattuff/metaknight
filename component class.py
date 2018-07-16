75864b9ece699ee2ca55f4e4ab22c7188df2c019
yaelEisenberg <eisenbergyael@gmail.com>
2018-07-16 10:10:25 -0700

    Update component class.py

--- component class.py
+++ component class.py
@@ -3,6 +3,9 @@ class component:
     def __init__(self, handle, framing=None): #for 1-handles, framing=infinity
         self.handle = handle
         self.framing = framing
+        
+    def __str__(self): #prints out component in nice way
+        return('handle: ' + str(self.handle) + ', framing: ' + str(self.framing))
 
     def get_handle(self): #returns type of handle: one or two handle
         return self.handle
