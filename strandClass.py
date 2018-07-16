f9f4b4c31960a68c46146c1d1e6cc5b2baffd4a8
yaelEisenberg <eisenbergyael@gmail.com>
2018-07-16 14:44:25 -0700

    took out get_succ, get_pred

--- strandClass.py
+++ strandClass.py
@@ -12,15 +12,9 @@ class strand:
     def set_succ(self,succ): #set the successor of this strand to be the strand succ
         self.succ=succ
         
-    def get_succ(self): #return the successor of this strand
-        return(self.succ)
-        
     def set_pred(self,pred): #set the predecessor of this strand to be the strand succ
         self.pred=pred
         
-    def get_pred(self): #return the successor of this strand
-        return(self.pred)
-    
     def set_component(self,comp): #set the component to which this strand belongs to be the component comp
         self.comp=comp
     
