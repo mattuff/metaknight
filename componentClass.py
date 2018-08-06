4acf692712b5444249171a6675db5e4b4a49842e
mattuff <mpu@umail.ucsb.edu>
2018-08-05 22:25:52 -0700

    components now have [] (slice operator)

--- componentClass.py
+++ componentClass.py
@@ -11,6 +11,10 @@ class component:
         
     def __str__(self): #prints out component in nice way
         return('handle: ' + str(self.handle) + ', framing: ' + str(self.framing))
+    
+    def __getitiem__(self,key):
+        return(list(filter(lambda x:x.component==s.component,k.strands))[key])
+
 
     def get_handle(self): #returns type of handle: one or two handle
         return self.handle
