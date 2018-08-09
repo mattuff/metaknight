646cd2a2d0c6061560fb2d373709cd9158ab6bbc
Laura Stordy <laurastordy@airbears2-10-142-117-195.airbears2.1918.berkeley.edu>
2018-08-09 13:28:50 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="MetaKnight",
     version="0.0.1",
-    author="Yael Eisenberg, Laura Stordy, Matt Uffenheimer",
+    author="Yael Eisenberg, Laura Stordy, Matthew Uffenheimer",
     author_email="laurastordy@gmail.com",
     description="A Python package for Kirby Calculus",
     long_description=long_description,
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/mattuff/KirbyCalculus",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         ),
 )
