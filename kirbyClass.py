f2a2d2839cd5a658e5e82430619f43dfa6a65502
yaelEisenberg <eisenbergyael@gmail.com>
2018-08-21 21:53:55 -0400

    r3

--- kirbyClass.py
+++ kirbyClass.py
@@ -365,7 +365,7 @@ class Kirby:
          else c2.set_strands(strandUnder, strand1, strandUnder.succ, strand2)
 
       #redefine c1
-      if (crTest(strandMiddle.pred, strandOver, c1)): crossSet(strandMiddle.pred.pred,strandMiddle.pred, 1)
+      if (crTest(strandMiddle.pred, strandOver, c1)): crossSet(strandMiddle.pred,strandMiddle.pred.pred, 1)
       elif (crTest(strandOver.pred, strandOver, c2)): crossSet(strandOver.pred.pred, strandOver.pred, 1)
       elif (crTest(strandMiddle.succ, strandOver, c1)): crossSet(strandMiddle.succ, strandMiddle.succ.succ, 1)
       elif (crTest(strandOver.succ, strandOver, c2)): crossSet(strandOver.succ.succ, strandOver.succ, 1)
@@ -373,7 +373,7 @@ class Kirby:
       #redefine c2 - use oldC1 since c1 gets redefined above
       if (crTest(strandMiddle.pred, strandOver, c2)): crossSet(strandMiddle.pred.pred, strandMiddle.pred, 2)
       elif (crTest(strandOver.pred, strandOver, oldC1)): crossSet(strandOver.pred, strandOver.pred.pred, 2)
-      elif (crTest(strandMiddle.succ, strandOver, c2)): crossSet(strandMiddle.succ, strandMiddle.succ.succ, 2)
+      elif (crTest(strandMiddle.succ, strandOver, c2)): crossSet(strandMiddle.succ.succ, strandMiddle.succ, 2)
       elif (crTest(strandOver.succ, strandOver, oldC1)): crossSet(strandOver.succ, strandOver.succ.succ, 2)
 
       #setting succ_pred/succ_con
